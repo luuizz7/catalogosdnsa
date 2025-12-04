9adfb6c5eeb4bb83a9824cc51783eaa75376c9ad
luuizz7 <saladalinex10@gmail.com>
2025-12-04 08:18:32 -0300

    redirect

--- middleware.js
+++ middleware.js
@@ -1,14 +1,13 @@
 export function middleware(req) {
   const allowedIP = "177.52.244.45"; // seu ip
-  const forwarded = req.headers.get("x-forwarded-for");
+  const visitorIP = req.ip || req.headers.get("x-forwarded-for");
 
-  // pega o primeiro ip da lista x-forwarded-for
-  const visitorIP = forwarded?.split(",")[0].trim();
-
+  // se o ip NAO for o seu, redireciona para outro site
   if (visitorIP !== allowedIP) {
-    return new Response("acesso restrito", { status: 403 });
+    return Response.redirect("https://google.com", 302); 
   }
 
+  // se for seu ip, deixa entrar normalmente
   return;
 }
 
