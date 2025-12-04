2e95ebdef376abf40e6453cea7b9c5af0c2e9a52
luuizz7 <saladalinex10@gmail.com>
2025-12-04 08:22:29 -0300

    xa

--- middleware.js
+++ middleware.js
@@ -1,13 +1,11 @@
 export function middleware(req) {
-  const allowedIP = "177.52.244.45"; // seu ip
+  const allowedIP = "177.52.244.45";
   const visitorIP = req.ip || req.headers.get("x-forwarded-for");
 
-  // se o ip NAO for o seu, redireciona para outro site
   if (visitorIP !== allowedIP) {
-    return Response.redirect("https://google.com", 302); 
+    return new Response("acesso restrito", { status: 403 });
   }
 
-  // se for seu ip, deixa entrar normalmente
   return;
 }
 
