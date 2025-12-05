b8baf48aa75417706f58ac2562731f894cb71978
luuizz7 <saladalinex10@gmail.com>
2025-12-05 07:31:56 -0300

    ll

--- meu-site/app/pages/api/ip.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  const visitorIP =
-    req.headers['x-real-ip'] ||
-    req.headers['x-forwarded-for'] ||
-    req.socket.remoteAddress ||
-    "";
-
-  res.status(200).json({ ip: visitorIP });
-}
